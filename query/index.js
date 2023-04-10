fbf10a2e53b338378f4135db80698cea997c1107
itzVort3x1 <saikaustubh10@gmail.com>
2023-04-10 07:11:13 +0530

    inital commit

---
+++ query/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+// posts === {
+//      'afjsd90w': {
+//           id: 'afjsd90w',
+//           title: 'Post Title',
+//           comments: [
+//                { id: 'aw23r90w', content: 'Comment #1' },
+//           ]
+//      }
+// }
+
+app.get("/posts", (req, res) => {
+	res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+	const { type, data } = req.body;
+
+	if (type === "PostCreated") {
+		const { id, title } = data;
+
+		posts[id] = { id, title, comments: [] };
+	}
+
+	if (type === "CommentCreated") {
+		const { id, content, postId, status } = data;
+
+		const post = posts[postId];
+
+		post.comments.push({ id, content, status });
+		console.log(posts);
+	}
+
+	res.send({});
+});
+
+app.listen(4002, () => {
+	console.log("Listening on 4002");
+});
