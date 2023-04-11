30ddb53edc7e580f6c7511772ae55707bdef05fb
itzVort3x1 <saikaustubh10@gmail.com>
2023-04-11 06:23:06 +0530

    added moderation service

--- query/index.js
+++ query/index.js
@@ -39,6 +39,18 @@ app.post("/events", (req, res) => {
 		console.log(posts);
 	}
 
+	if (type === "CommentUpdated") {
+		const { id, content, postId, status } = data;
+
+		const post = posts[postId];
+		const comment = post.comments.find((comment) => {
+			return comment.id === id;
+		});
+
+		comment.status = status;
+		comment.content = content;
+	}
+
 	res.send({});
 });
 
